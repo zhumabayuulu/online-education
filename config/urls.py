6bda802f22d6e816e42ba8b916a327e5d8566d36
zhumabayuulu <zhumabay>
2023-04-09 23:25:24 +0300

    new

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
                   path('blog_nature/', include('blog_nature.urls')),
                 #path('accounts/', include('django.contrib.auth.urls')),
                   path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
