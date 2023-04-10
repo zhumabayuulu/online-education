604a02e84331fa280e667b35ff4e1f24bc5a95bd
zhumabayuulu <zhumabay>
2023-04-10 22:38:19 +0300

    new

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
