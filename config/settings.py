3b65f4c8a23ed54787441d2927e2128cc20b1a93
zhumabayuulu <zhumabay>
2023-04-19 13:31:35 +0300

    update root

--- config/settings.py
+++ config/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")),)
-STATIC_ROOT = os.path.join(BASE_DIR,'static-files')
+STATIC_ROOT = 'static-files'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
+MEDIA_ROOT = "media-files"
 
 
 # Default primary key field type
