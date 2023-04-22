57afd3b9bf7b8514546c05f58c063235e199693d
zhumabayuulu <zhumabay>
2023-04-22 20:40:20 +0300

    add media root

--- config/settings.py
+++ config/settings.py
@@ -138,9 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")),)
-STATIC_ROOT = '/static'
+STATIC_ROOT = 'static-files'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = "/media"
+MEDIA_ROOT = "media-files"
 
 
 # Default primary key field type
