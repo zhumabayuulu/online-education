6e5f730b40e3a20c8f839f491025d50a4fbd2dab
zhumabayuulu <zhumabay>
2023-04-29 22:42:57 +0300

    store page rename

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
                   path('blog_admin/', include('blog_admin.urls')),
                   path('blog_nature/', include('blog_nature.urls')),
                   path('accounts/', include('accounts.urls')),
-]
+
+    ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
