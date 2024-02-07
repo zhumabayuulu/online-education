c0df9ef2e0763e2044154e838b5d6ce33c3390c4
almanbet <103384218+zhumabayuulu@users.noreply.github.com>
2024-02-07 16:53:35 +0600

    pp

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,10 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 # Password validation
